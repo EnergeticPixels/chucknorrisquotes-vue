5d7361a05859ef10d92fb36f7a07c382cbfd9001
Jackman <Anthony.Jackman@serco-na.com>
2017-12-07 15:08:57 -0600

    Signup component

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import Login from './components/Login.vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 
-import auth from './auth';
-// check the users auth status when the app starts
-auth.checkAuth();
-
 Vue.use(VueResource);
 Vue.use(VueRouter);
+
+import auth from './auth';
+
+Vue.http.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token');
+// check the users auth status when the app starts
+auth.checkAuth();
 
 export var router = new VueRouter();
 
